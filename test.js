67b8dc388f9c171a2b60516852e51872ae7607f7
ScriptSmith <ScriptSmith@users.noreply.github.com>
2017-03-29 14:07:35 +1000

    fixed url

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var app = express()
 
 var port = process.env.PORT || 8080;
 
-app.get('/well-known/acme-challenge/SImgd24WRS64hT8QsPGFVLay1nmGM_IO6WLrBGvr9QA', function(req, res) {
+app.get('/.well-known/acme-challenge/SImgd24WRS64hT8QsPGFVLay1nmGM_IO6WLrBGvr9QA', function(req, res) {
     res.send('SImgd24WRS64hT8QsPGFVLay1nmGM_IO6WLrBGvr9QA.4UzW-_pc32ZB01RhhqCtjeRfzy6vbluKquABtpYww-Q')
 })
 
