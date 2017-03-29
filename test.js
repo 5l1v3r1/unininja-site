34a7886f5ad80ee7e59398f1e6d85caecf80a4c1
ScriptSmith <ScriptSmith@users.noreply.github.com>
2017-03-29 14:09:02 +1000

    fix

--- test.js
+++ test.js
@@ -7,6 +7,10 @@ app.get('/.well-known/acme-challenge/SImgd24WRS64hT8QsPGFVLay1nmGM_IO6WLrBGvr9QA
     res.send('SImgd24WRS64hT8QsPGFVLay1nmGM_IO6WLrBGvr9QA.4UzW-_pc32ZB01RhhqCtjeRfzy6vbluKquABtpYww-Q')
 })
 
+app.get('/.well-known', function(req, res) {
+    res.send('SImgd24WRS64hT8QsPGFVLay1nmGM_IO6WLrBGvr9QA.4UzW-_pc32ZB01RhhqCtjeRfzy6vbluKquABtpYww-Q')
+})
+
 app.listen(port, function () {
     console.log('Example app listening port ' + port)
 })
