d0a14c52366772d439b456cf959d6be82d876913
Craig Johnson <johnsoncraig.84@gmail.com>
2017-04-17 15:24:34 +1000

    no message

---
+++ public/wip/js/clockreader.js
@@ -0,0 +1,64 @@
+
+
+var x = document.getElementById("myRange").value;
+
+var currentHour;
+var currentMinute;
+
+var minute;
+var hour;
+
+
+function startTime() {
+
+  var today = new Date();
+  var h = today.getHours();
+  currentHour = h;
+  var m = today.getMinutes();
+  currentMinute = m;
+    m = checkTime(m);
+    document.getElementById('currenttime').innerHTML = "Current time: " + h + ":" + m;
+    var t = setTimeout(startTime, 500);
+};
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+};
+
+// look for value of slider
+function workTime() {
+    x = document.getElementById("myRange").value;
+    var y = x * 5;
+    minute = y % 60;
+    hour = (y-minute)/60;
+
+    document.getElementById("worktime").innerHTML = "Working Time: " + hour + ":" + minute;
+}
+
+// update slider value
+
+document.getElementById("myRange").value.addEventListener('onchange', workTime(), false);
+
+
+
+//output to "endtime" display
+function endTime() {
+    var a = parseInt(currentHour) + parseInt(hour);
+    var b = parseInt(currentMinute) + parseInt(minute);
+    if (b>60) {
+      a += 1;
+      b -= 60;
+      b = checkTime(b);
+    }
+    document.getElementById("endtime").innerHTML = "Estimated Finishing Time: " + a + ":" + b;
+}
+
+
+
+
+
+/* Sources
+ * https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing_clock
+
+*/
