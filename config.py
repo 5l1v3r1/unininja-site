61d5f834e9911a6fbe2a25e92325d3e46007d7bd
Adam Smith <adamdevsmith@gmail.com>
2019-02-06 12:55:35 +1000

    database config

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     OAUTHLIB_INSECURE_TRANSPORT = 1
-    SQLALCHEMY_DATABASE_URI = 'postgres://mwbmjfsrxqwulo:5c962ac9681facb8459cd7be573dcfc4262a4e10d2ba8d989f5dcae100a8b0d6@ec2-54-243-252-91.compute-1.amazonaws.com:5432/d2hdijpk4h5m4v'
+    SQLALCHEMY_DATABASE_URI = 'DROPIN_URL'
 
 
 class DevConfig(Config):
